782a40aaeafc38f1940c0acc2dcf56a6d1574839
thierryH91200 <thierry.hentic@gmail.com>
2020-02-08 14:59:06 +0100

    Initial Commit

---
+++ THDate/THDate.h
@@ -0,0 +1,19 @@
+//
+//  THDate.h
+//  THDate
+//
+//  Created by thierry hentic on 08/02/2020.
+//  Copyright © 2020 thierry hentic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for THDate.
+FOUNDATION_EXPORT double THDateVersionNumber;
+
+//! Project version string for THDate.
+FOUNDATION_EXPORT const unsigned char THDateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THDate/PublicHeader.h>
+
+
